61cf64f75fdfb55d9b03243bc4a632ae1d4830b7
yalibai <108649734+yali-bai@users.noreply.github.com>
2025-06-25 09:50:27 +0800

    Add files via upload

--- 01.Young_Mouse/03.subclass.DMR/01-wgbstools_generate_segment/03-get_segment_bed.sh
+++ 01.Young_Mouse/03.subclass.DMR/01-wgbstools_generate_segment/03-get_segment_bed.sh
@@ -4,7 +4,7 @@
 # indir=""
 # outdir=""
 
-cat ${indir}/tso.5hmc.segment.3cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/01-youth/03-wgbstools_generate_segment/TSO_5hmC_segment_3CpG.bed
-cat ${indir}/tso.5hmc.segment.5cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/01-youth/03-wgbstools_generate_segment/TSO_5hmC_segment_5CpG.bed
-cat ${indir}/tso.5mc.segment.3cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/01-youth/03-wgbstools_generate_segment/TSO_5mC_segment_3CpG.bed
-cat ${indir}/tso.5mc.segment.5cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/01-youth/03-wgbstools_generate_segment/TSO_5mC_segment_5CpG.bed
+cat ${indir}/tso.5hmc.segment.3cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/01.Young_Mouse/03-wgbstools_generate_segment/TSO_5hmC_segment_3CpG.bed
+cat ${indir}/tso.5hmc.segment.5cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/01.Young_Mouse/03-wgbstools_generate_segment/TSO_5hmC_segment_5CpG.bed
+cat ${indir}/tso.5mc.segment.3cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/01.Young_Mouse/03-wgbstools_generate_segment/TSO_5mC_segment_3CpG.bed
+cat ${indir}/tso.5mc.segment.5cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/01.Young_Mouse/03-wgbstools_generate_segment/TSO_5mC_segment_5CpG.bed
