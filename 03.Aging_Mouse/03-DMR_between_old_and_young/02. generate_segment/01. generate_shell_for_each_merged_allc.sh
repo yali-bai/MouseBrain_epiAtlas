2b7f1697673c11c9dff58203891af2a1a53bd2f8
yalibai <108649734+yali-bai@users.noreply.github.com>
2025-06-27 16:46:22 +0800

    Add files via upload

--- 03.Aging_Mouse/03-DMR_between_old_and_young/02. generate_segment/01. generate_shell_for_each_merged_allc.sh
+++ 03.Aging_Mouse/03-DMR_between_old_and_young/02. generate_segment/01. generate_shell_for_each_merged_allc.sh
@@ -1,7 +1,7 @@
 # "indir" is a custom input path.
 # indir=""
 
-ls ../../../output/03-aging/02-merge_allc/*tsv.gz|sed 's/merge_allc\///g'|while read a;do 
+ls ../../../output/03.Aging_Mouse/02-merge_allc/*tsv.gz|sed 's/merge_allc\///g'|while read a;do 
     echo "#!/bin/bash" > run.$a.sh
     echo "less merge_allc/$a | awk '\$4 ~ /^CG/' > $a.cg.txt" >> run.$a.sh
     echo "python ${indir}/merge.strand.py $a.cg.txt $a.cg.txt.bed" >> run.$a.sh 
--- 03.Aging_Mouse/03-DMR_between_old_and_young/02. generate_segment/03. get_segment_bed.sh
+++ 03.Aging_Mouse/03-DMR_between_old_and_young/02. generate_segment/03. get_segment_bed.sh
@@ -6,5 +6,5 @@
 
 # start is 0-based, end is 1-based
 
-cat ${indir}/tso.5hmc.segment.3cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/03-aging/03-DMRs_DHMRs/segments/tso.5hmC.segment.3cpg.0_base.bed
-cat ${indir}/tso.5mc.segment.3cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/03-aging/03-DMRs_DHMRs/segments/tso.5mC.segment.3cpg.0_base.bed
+cat ${indir}/tso.5hmc.segment.3cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/03.Aging_Mouse/03-DMRs_DHMRs/segments/tso.5hmC.segment.3cpg.0_base.bed
+cat ${indir}/tso.5mc.segment.3cpg.bed | awk -v OFS="\t" '{print $1,$2-1,$3,$1"_"$2-1"_"$3}' > ../../../output/03.Aging_Mouse/03-DMRs_DHMRs/segments/tso.5mC.segment.3cpg.0_base.bed
